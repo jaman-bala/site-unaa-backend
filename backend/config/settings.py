8d70baeb4c4db9fc95878458b60d9c59c0a4871c
jaman-bala <ermekoffdastan@yandex.ru>
2024-04-14 14:48:45 +0600

    up:47 add CSRF_TRUSTED_ORIGINS

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,8 +5,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
+CSRF_TRUSTED_ORIGINS = ['http://tsvs.gov.kg']
+
 
 # Application definition
 
