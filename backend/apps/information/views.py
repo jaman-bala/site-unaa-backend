a01f70e68f0645b10b2fe6b9a77eaa73a22a8d84
jaman-bala <ermekoffdastan@yandex.ru>
2024-04-19 14:37:18 +0600

    up:67 clear cache

--- backend/apps/information/views.py
+++ backend/apps/information/views.py
@@ -1,8 +1,5 @@
 from typing import List
-from django.http import JsonResponse
 from ninja import Router
-from django.views.decorators.cache import cache_page
-from django.core.cache import cache
 
 from backend.apps.information.models import Register_Car01, Register_Car02, Issue_Car01, Issue_Car02, Confirmation
 from backend.apps.information.schemas import RegisterCar01, RegisterCar02, IssueCar01, IssueCar02, ConfirmationOut
@@ -11,58 +8,21 @@ router = Router()
 
 
 @router.get("/info01", response=List[RegisterCar01])
-@cache_page(60 * 15)  # кэшировать страницу на 15 минут
 def get_register01(request):
-    cached_data = cache.get('register01_data')
-    if cached_data is None:
-        qs = Register_Car01.objects.all()
-        cached_data = [RegisterCar01.from_orm(info01).dict() for info01 in qs]
-        cache.set('register01_data', cached_data, timeout=60 * 15)  # кэшировать на 15 минут
-    return JsonResponse(cached_data, safe=False)
+    qs = Register_Car01.objects.all()
+    return [RegisterCar01.from_orm(info01).dict() for info01 in qs]
 
 
 @router.get("/info02", response=List[RegisterCar02])
-@cache_page(60 * 15)  # кэшировать страницу на 15 минут
 def get_register02(request):
-    cached_data = cache.get('register02_data')
-    if cached_data is None:
-        qs = Register_Car02.objects.all()
-        cached_data = [RegisterCar02.from_orm(info02).dict() for info02 in qs]
-        cache.set('register02_data', cached_data, timeout=60 * 15)  # кэшировать на 15 минут
-    return JsonResponse(cached_data, safe=False)
+    qs = Register_Car02.objects.all()
+    return qs
 
 
 @router.get("/issu01", response=List[IssueCar01])
-@cache_page(60 * 15)  # кэшировать страницу на 15 минут
 def get_issu01(request):
-    cached_data = cache.get('issue01_data')
-    if cached_data is None:
-        qs = Issue_Car01.objects.all()
-        cached_data = [IssueCar01.from_orm(issue01).dict() for issue01 in qs]
-        cache.set('issue01_data', cached_data, timeout=60 * 15)  # кэшировать на 15 минут
-    return JsonResponse(cached_data, safe=False)
-
-
-@router.get("/issu02", response=List[IssueCar02])
-@cache_page(60 * 15)  # кэшировать страницу на 15 минут
-def get_issu02(request):
-    cached_data = cache.get('issue02_data')
-    if cached_data is None:
-        qs = Issue_Car02.objects.all()
-        cached_data = [IssueCar02.from_orm(issue02).dict() for issue02 in qs]
-        cache.set('issue02_data', cached_data, timeout=60 * 15)  # кэшировать на 15 минут
-    return JsonResponse(cached_data, safe=False)
-
-
-@router.get("/conf", response=List[ConfirmationOut])
-@cache_page(60 * 15)  # кэшировать страницу на 15 минут
-def get_conf(request):
-    cached_data = cache.get('confirmation_data')
-    if cached_data is None:
-        qs = Confirmation.objects.all()
-        cached_data = [ConfirmationOut.from_orm(conf).dict() for conf in qs]
-        cache.set('confirmation_data', cached_data, timeout=60 * 15)  # кэшировать на 15 минут
-    return JsonResponse(cached_data, safe=False)
+    qs = Issue_Car01.objects.all()
+    return qs
 
 
 @router.get("/issu02", response=List[IssueCar02])
--- backend/config/settings.py
+++ backend/config/settings.py
@@ -49,9 +49,6 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'django.middleware.cache.UpdateCacheMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.cache.FetchFromCacheMiddleware',
 ]
 
 CORS_ALLOWED_ORIGINS = [
