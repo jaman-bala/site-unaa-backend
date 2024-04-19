31e87523f124a862bc38d844d5dc053e751d72a9
jaman-bala <ermekoffdastan@yandex.ru>
2024-04-19 14:07:31 +0600

    up:66 edit cache

--- backend/apps/index/views.py
+++ backend/apps/index/views.py
@@ -1,9 +1,6 @@
-from django.core.cache import cache
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from typing import List
 from ninja import Router
-from django.views.decorators.cache import cache_page
 
 from backend.apps.index.schemas import IndexSchema, IndexOUT
 from backend.apps.index.models import Index
@@ -12,23 +9,12 @@ router = Router()
 
 
 @router.get("/video", response=List[IndexSchema])
-@cache_page(60 * 30)
-def get_all_index(request):
-    cached_data = cache.get('all_index_data')
-    if cached_data is None:
-        qs = Index.objects.all()
-        cached_data = [IndexOUT.from_orm(video).dict() for video in qs]
-        cache.set('all_index_data', cached_data, timeout=60 * 30)
-    return JsonResponse(cached_data, safe=False)
+def get_all_sliders(request):
+    qs = Index.objects.all()
+    return qs
 
 
 @router.get("/video/{index_pk}", response=IndexOUT)
-@cache_page(60 * 30)
-def get_index(request, index_pk: int):
-    cache_key = f'index_{index_pk}'
-    cached_data = cache.get(cache_key)
-    if cached_data is None:
-        video = get_object_or_404(Index, id=index_pk)
-        cached_data = IndexOUT.from_orm(video).dict()
-        cache.set(cache_key, cached_data, timeout=60 * 30)
-    return JsonResponse(cached_data)
+def get_slider(request, slider_pk: int):
+    video = get_object_or_404(Index, id=slider_pk)
+    return video
--- backend/apps/news/views.py
+++ backend/apps/news/views.py
@@ -1,8 +1,5 @@
-from django.core.cache import cache
-from django.http import JsonResponse
 from django.shortcuts import get_object_or_404
 from typing import List
-from django.views.decorators.cache import cache_page
 from ninja import Router
 
 from backend.apps.news.schemas import NewsOUT
@@ -18,14 +15,7 @@ def get_all_news(request):
 
 
 @router.get("/news/{news_pk}", response=NewsOUT)
-@cache_page(60 * 60 * 20)
 def get_news(request, news_pk: int):
-    cache_key = f'news_{news_pk}'
-    cached_news = cache.get(cache_key)
-    if cached_news is None:
-        news = get_object_or_404(News, id=news_pk)
-        cached_news = NewsOUT.from_orm(news).dict()
-        cache.set(cache_key, cached_news, timeout=60 * 60 * 20)
-    return JsonResponse(cached_news)
-
+    news = get_object_or_404(News, id=news_pk)
+    return NewsOUT.from_orm(news).dict()
 
