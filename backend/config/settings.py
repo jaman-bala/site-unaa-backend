371b30a1e8f380228b439d1c511b157377c121d0
jaman-bala <ermekoffdastan@yandex.ru>
2024-04-26 19:33:25 +0600

    up:70 DEBUG=False

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -5,7 +5,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 ALLOWED_HOSTS = ["*"]
 CSRF_TRUSTED_ORIGINS = [
     'http://10.11.13.101:8080',
