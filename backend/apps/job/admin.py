b6cd5f54b7816d335e31cb8410b5cc5cef1856cf
jaman-bala <ermekoffdastan@yandex.ru>
2024-04-29 09:28:03 +0600

    up:72 admin list_display

--- backend/apps/job/admin.py
+++ backend/apps/job/admin.py
@@ -6,3 +6,4 @@ from backend.apps.job.models import Job
 @admin.register(Job)
 class JobAdmin(admin.ModelAdmin):
     list_display = ('title_ru', 'city', 'note_ru', 'created_date', 'is_active')
+    search_fields = ('title_ru',)
